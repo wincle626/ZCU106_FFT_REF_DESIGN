808f2f92ffe7dac7ed3d64db628aa43be5b8633a
Yun Wu <wincle626@users.noreply.github.com>
2021-05-13 22:25:22 +0100

    add files

---
+++ software/zcu106_fft/Debug/src/outbyte.d
@@ -0,0 +1,27 @@
+src/outbyte.o src/outbyte.o: ../src/outbyte.c ../src/xparameters.h \
+ ../src/xparameters_ps.h ../src/xuartps_hw.h ../src/xil_types.h \
+ ../src/xil_assert.h ../src/xil_io.h ../src/xil_printf.h \
+ ../src/bspconfig.h ../src/xpseudo_asm.h ../src/xreg_cortexa53.h \
+ ../src/xpseudo_asm_gcc.h
+
+../src/xparameters.h:
+
+../src/xparameters_ps.h:
+
+../src/xuartps_hw.h:
+
+../src/xil_types.h:
+
+../src/xil_assert.h:
+
+../src/xil_io.h:
+
+../src/xil_printf.h:
+
+../src/bspconfig.h:
+
+../src/xpseudo_asm.h:
+
+../src/xreg_cortexa53.h:
+
+../src/xpseudo_asm_gcc.h:
